46ab32cc0588e7aab7f764922cdf27b0272dfea5
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-10-17 11:27:33 +0200

    Add files via upload

---
+++ galavićMarkoKreiranjeProjekta/galavićMarkoKreiranjeProjekta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoKreiranjeProjekta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            float y = 50.45F;
+            int x;
+            
+            x = Convert.ToInt32(y);
+   
+
+            Console.WriteLine("y= "+y+"dolara.");
+            Console.WriteLine("x= {0} rublji.A y= {1}",x,y);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
